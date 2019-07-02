3b392bfcb6e5552d059b8094594db053fe9d5254
priyankagiri2614 <priyankagiri@github.com>
2019-07-02 12:24:12 +0530

    No updates

--- app/src/main/java/com/ubits/payflow/payflow_network/Web_Services/IBaseURL.java
+++ app/src/main/java/com/ubits/payflow/payflow_network/Web_Services/IBaseURL.java
@@ -1,5 +1,5 @@
 package com.ubits.payflow.payflow_network.Web_Services;
 
 public interface IBaseURL {
-    String BASE_URL = "http://192.168.1.17/rica/";
+    String BASE_URL = "http://192.168.1.8/rica/";
 }
